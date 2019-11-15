64a12744ebe1d0b7580261c3631de54b83a65b48
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-15 15:57:29 -0300

    Finished Login

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,31 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import { signInSuccess, signFailure } from './actions';
+
+import api from '~/services/api';
+import history from '~/services/history';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+    const { token, user } = response.data;
+    if (!user.provider) {
+      toast.error('Usuario não é prestador');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
+    toast.error('Falha na autenticação, verifique seus dados');
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
